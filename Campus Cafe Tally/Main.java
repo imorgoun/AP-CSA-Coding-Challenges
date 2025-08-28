152779bebf2341181da703b91516228e0160f1da
Illya Morgun <i.morgoun@gmail.com>
2025-08-28 14:41:50 +0200

    Finished Campus Cafe Tally Coding Challenge

---
+++ Campus Cafe Tally/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    static double COFFEE_PRICE = 2.10;
+    static double TEA_PRICE = 1.60;
+    static double PASTRY_COST = 2.75;
+
+    static double VAT = 1.21;
+    static double SERVICE_FEE = 1.30;
+
+    public static void main(String[] args) {
+        int coffeeCount;
+        int teaCount;
+        int pastryCount;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter coffee count for coffee: ");
+        coffeeCount = scan.nextInt();
+
+        System.out.print("Enter coffee count for coffee: ");
+        teaCount = scan.nextInt();
+
+        System.out.print("Enter coffee count for coffee: ");
+        pastryCount = scan.nextInt();
+
+        scan.close();
+
+        double subTotal = (coffeeCount * COFFEE_PRICE) + (teaCount * TEA_PRICE) + (pastryCount * PASTRY_COST);
+        double total = subTotal * (SERVICE_FEE + VAT);
+
+        System.out.println(total);
+    }
+}
